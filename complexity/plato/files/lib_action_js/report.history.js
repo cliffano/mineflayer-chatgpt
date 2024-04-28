159aab332beb4ae4e4ebc6d368d346d207892a0f
cliffano <cliffano@users.noreply.github.com>
2024-04-28 07:59:18 +0000

    deploy: 98d43ca6738f1f20e6822b99152590e651a2d2d5

--- complexity/plato/files/lib_action_js/report.history.js
+++ complexity/plato/files/lib_action_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 28 Apr 2024 07:58:40 GMT","sloc":39,"lloc":14,"functions":1,"deliveredBugs":0.148,"difficulty":11.407,"maintainability":62.731,"lintErrors":6}]
+__history = [{"date":"Sun, 28 Apr 2024 07:59:14 GMT","sloc":39,"lloc":14,"functions":1,"deliveredBugs":0.148,"difficulty":11.407,"maintainability":62.731,"lintErrors":6}]
--- complexity/plato/files/lib_reference_js/report.history.js
+++ complexity/plato/files/lib_reference_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sun, 28 Apr 2024 07:58:40 GMT","sloc":21,"lloc":6,"functions":3,"deliveredBugs":0.046,"difficulty":8.444,"maintainability":90.466,"lintErrors":2}]
+__history = [{"date":"Sun, 28 Apr 2024 07:59:14 GMT","sloc":21,"lloc":6,"functions":3,"deliveredBugs":0.046,"difficulty":8.444,"maintainability":90.466,"lintErrors":2}]
